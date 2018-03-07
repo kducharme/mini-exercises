684148bdf7f1ee51587f6c97ebc498ec1bef9f3f
Kyle Ducharme <ducharme.kyle@gmail.com>
2018-03-06 20:51:54 -0600

    regrouped files

---
+++ random/random.js
@@ -0,0 +1,27 @@
+// Function needs to accept lower and upper number
+// Generate random number between the 2
+
+function randomNumber(low, high) {
+    if (isNaN(low) || isNaN(high)) {
+        alert("One of the inputs is not a number")
+    }
+    let randomNum = Math.floor(Math.random() * (high - low + 1)) + low;
+    console.log(randomNum)
+}
+
+function submitInput() {
+    let low = document.getElementById("input1").value
+    let high = document.getElementById("input2").value
+    randomNumber(parseInt(low), parseInt(high))
+    countUntilHigh(parseInt(low), parseInt(high))
+}
+
+function countUntilHigh(low, high) {
+    let printCount = low;
+    for (var i = low; i < high - 1; i++) {
+        printCount++
+        console.log(printCount)
+    }
+}
+
+document.getElementById("submit-button").addEventListener("click", submitInput)
